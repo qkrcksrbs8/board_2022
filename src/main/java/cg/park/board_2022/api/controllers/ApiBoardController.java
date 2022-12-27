0500260e6bd9350b538d79ec0e7f56dbe8b1acec
pcg0902 <qkrcksrbs8@naver.com>
2022-12-27 10:33:47 +0900

    어노테이션 구현

--- src/main/java/cg/park/board_2022/api/controllers/ApiBoardController.java
+++ src/main/java/cg/park/board_2022/api/controllers/ApiBoardController.java
@@ -1,5 +1,6 @@
 package cg.park.board_2022.api.controllers;
 
+import cg.park.board_2022.comm.annotation.RequireLogin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiBoardController {
 
+    @RequireLogin
     @GetMapping("/test")
     public String index() {
         return "test";
--- src/main/java/cg/park/board_2022/comm/aop/LoginInterceptor.java
+++ src/main/java/cg/park/board_2022/comm/aop/LoginInterceptor.java
@@ -1,9 +1,20 @@
 package cg.park.board_2022.comm.aop;
 
+import cg.park.board_2022.comm.annotation.RequireLogin;
+import com.sun.istack.NotNull;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 @Aspect
@@ -11,7 +22,28 @@ public class LoginInterceptor {
 
     Logger logger = LoggerFactory.getLogger(super.getClass());
 
+//    @Around("execution(* cg.park.board_2022..controllers.*.*(..))")
+    @Around("@annotation(cg.park.board_2022.comm.annotation.RequireLogin)")
+    public Object around(@NotNull ProceedingJoinPoint joinPoint) throws Throwable {
+        String type = joinPoint.getSignature().toShortString();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        request.getParameterMap();
 
+
+//        ModelAndView mv = new ModelAndView();
+//        String token = request.getHeader("Authorization");
+//        if (null == token) return
+
+
+//        ModelAndView mv = new ModelAndView();
+//        mv.setViewName("redirect:/auth/login?nextUrl=" + URLEncoder.encode(url, "UTF-8"));
+
+        logger.info("SESS_GUID = {}, ===================START===================");
+        logger.info("SESS_GUID = {}, @Around : {}, param : {} ", type);
+//        logger.info("SESS_GUID = {}, ===================START===================", sessGuid);
+//        logger.info("SESS_GUID = {}, @Around : {}, param : {} ", sessGuid, type, logUtil.mapToStr(request.getParameterMap()));
+        return joinPoint.proceed();
+    }
 
 
 }
