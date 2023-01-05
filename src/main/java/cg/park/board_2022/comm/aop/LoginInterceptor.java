a32a7cdaf5bd569e7ca87d2f5c9c032206d9e31f
pcg0902 <qkrcksrbs8@naver.com>
2023-01-05 10:49:57 +0900

    로그인 인터셉터 수정 중

--- src/main/java/cg/park/board_2022/comm/aop/LoginInterceptor.java
+++ src/main/java/cg/park/board_2022/comm/aop/LoginInterceptor.java
@@ -1,12 +1,20 @@
 package cg.park.board_2022.comm.aop;
 
 import cg.park.board_2022.comm.annotation.RequireLogin;
+import cg.park.board_2022.comm.auth.JwtProvider;
+import cg.park.board_2022.comm.utils.Message;
+import cg.park.board_2022.comm.utils.Param;
+import cg.park.board_2022.comm.utils.PcgModelAndView;
 import com.sun.istack.NotNull;
+import io.jsonwebtoken.Claims;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -22,6 +30,12 @@ public class LoginInterceptor {
 
     Logger logger = LoggerFactory.getLogger(super.getClass());
 
+    @Autowired
+    Message message;
+
+    @Autowired
+    JwtProvider jwt;
+
 //    @Around("execution(* cg.park.board_2022..controllers.*.*(..))")
     @Around("@annotation(cg.park.board_2022.comm.annotation.RequireLogin)")
     public Object around(@NotNull ProceedingJoinPoint joinPoint) throws Throwable {
@@ -30,10 +44,19 @@ public class LoginInterceptor {
         request.getParameterMap();
 
 
-//        ModelAndView mv = new ModelAndView();
-//        String token = request.getHeader("Authorization");
-//        if (null == token) return
+        PcgModelAndView mv = new PcgModelAndView();
+        String token = request.getHeader("Authorization");
+        if (null == token) return mv.set("redirect:/api/loginTest");
 
+
+        try {
+            Claims claims = jwt.parseJwtToken(token);
+            Param param = new Param().set("claims", claims);
+//            return new ResponseEntity<>(message.success(param), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(message.notAuth(), HttpStatus.OK);
+        }
 
 //        ModelAndView mv = new ModelAndView();
 //        mv.setViewName("redirect:/auth/login?nextUrl=" + URLEncoder.encode(url, "UTF-8"));
